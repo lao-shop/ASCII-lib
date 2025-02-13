3cce3f7547bb860b2ef403289cc5c75bc6f58b3b
Lao Shop <lao_shop_n@proton.me>
2025-02-13 19:43:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from pyfiglet import Figlet
 import cowsay
 import random
 import os
 import time
 import colorama
 from colorama import init, Fore, Back, Style
+import pywhatkit
 
-def print_banner():
-    banner = Figlet(font='xcour')
-    print(banner.renderText('Matrix'))
+def exx():
+    print('0. Back                 99. Exit')
+    decide = int(input(Fore.BLUE + '[+]Enter number of function: '))
+
+    if decide == 99:
+        print(Fore.RED + '[+]Program finished')
+        exit()
+    elif decide == 0:
+        os.system('cls')
+        main(decide)
 
 decide = 0
 
@@ -19,16 +26,17 @@ def main(decide):
 '`^^^^^^^^^^^^ /====================================="\n'
             '            \/\n')
     print(Fore.RED + '===============================================================\n'
-          '               made by https://github.com/lao-py\n'
+          '               made by https://t.me/lao_takanava\n'
           '===============================================================\n'
           '\n'
           '1. Matrix               2. Ghostbusters banner\n'
           '3. Tiger banner         4. Turkey banner\n'
           '5. Linux penguin banner 6. Author\n'
           '7. +REP                 8. -REP\n'
+          '9. Convert image to ASCII-art\n'
           '99. Exit\n')
 
-    decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
+    decide = int(input(Fore.BLUE + '[+]Enter number of function: '))
 
     if decide == 1:
         while decide == 1:
@@ -41,71 +49,30 @@ def main(decide):
 
     elif decide == 2:
         cowsay.ghostbusters('Ghostbusters!')
-        print('0. Back                 99. Exit')
-        decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
-
-        if decide == 99:
-            print(Fore.RED + '[l@osint]Program finished')
-            exit()
-        elif decide == 0:
-            os.system('cls')
-            main(decide)
+        exx()
 
 
     elif decide == 3:
         cowsay.meow('Meow')
-        print('0. Back                 99. Exit')
-        decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
-
-        if decide == 99:
-            print(Fore.RED + '[l@osint]Program finished')
-            exit()
-        elif decide == 0:
-            os.system('cls')
-            main(decide)
-
+        exx()
 
 
     elif decide == 4:
         cowsay.turkey('AAAAAAAAAAAAAAAA')
-        print('0. Back                 99. Exit')
-        decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
-
-        if decide == 99:
-            print(Fore.RED + '[l@osint]Program finished')
-            exit()
-        elif decide == 0:
-            os.system('cls')
-            main(decide)
+        exx()
 
 
     elif decide == 5:
         cowsay.tux('Hello world!')
-        print('0. Back                 99. Exit')
-        decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
-
-        if decide == 99:
-            print(Fore.RED + '[l@osint]Program finished')
-            exit()
-        elif decide == 0:
-            os.system('cls')
-            main(decide)
+        exx()
 
 
     elif decide == 6:
         print(Fore.MAGENTA + "I'm Lao Takanava. I'm starter python programmer. \n"
             "If you want you can subscribe my tg channels:\n"
             "https://t.me/c_lao_tg\n"
-            "https://t.me/laosint")
-        print('0. Back                 99. Exit')
-        decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
-
-        if decide == 99:
-            print(Fore.RED + '[l@osint]Program finished')
-            exit()
-        elif decide == 0:
-            os.system('cls')
-            main(decide)
+            "https://t.me/lao_shop")
+        exx()
 
 
     elif decide == 7:
@@ -115,15 +82,7 @@ def main(decide):
                 '╚═██╔═╝██╔══██╗██╔══╝░░██╔═══╝░░\n'
                 '░░╚═╝░░██║░░██║███████╗██║░░░░░░\n'
                 '░░░░░░░╚═╝░░╚═╝╚══════╝╚═╝░░░░░░\n')
-        print('0. Back                 99. Exit')
-        decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
-
-        if decide == 99:
-            print(Fore.RED + '[l@osint]Program finished')
-            exit()
-        elif decide == 0:
-            os.system('cls')
-            main(decide)
+        exx()
 
 
     elif decide == 8:
@@ -133,19 +92,24 @@ def main(decide):
                 '╚═════╝██╔══██╗██╔══╝░░██╔═══╝░░\n'
                 '░░░░░░░██║░░██║███████╗██║░░░░░░\n'
                 '░░░░░░░╚═╝░░╚═╝╚══════╝╚═╝░░░░░░\n')
-        print('0. Back                 99. Exit')
-        decide = int(input(Fore.BLUE + '[l@osint]Enter number of function: '))
+        exx()
 
-        if decide == 99:
-            print(Fore.RED + '[l@osint]Program finished')
-            exit()
-        elif decide == 0:
-            os.system('cls')
-            main(decide)
+
+    elif decide == 9:
+        image_path = input(Fore.BLUE + '[+]Enter path to image, that you want to convert into ASCII-art: ')
+        output_file = 'ascii-art.txt'
+        pywhatkit.image_to_ascii_art(image_path, output_file)
+        print(f'[+]ASCII-art saved to {output_file}')
+        exx()
 
 
     elif decide == 99:
-        print(Fore.RED + '[l@osint]Program finished')
+        print(Fore.RED + '[+]Program finished')
+        exit()
+
+
+    else:
+        print(Fore.RED + '[-]Wrong command, program finished')
         exit()
 
 main(decide)
